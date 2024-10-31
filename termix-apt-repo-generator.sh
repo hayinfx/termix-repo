d2500fff6c2dbcf1c5844e4c69b5526340a4af02
Hayin FX <hayinfx@yahoo.com>
2024-10-31 22:08:09 +0700

    update APT repository

---
+++ termix-apt-repo-generator.sh
@@ -0,0 +1,5 @@
+# code to generate termix apt repo
+
+termux-apt-repo static static/apt/termix-main stable main
+
+# repo url : https://hayinfx.github.io/termix-repo/apt/termix-main
--- termux_apt_repo.sh
+++
@@ -1,3 +0,0 @@
-termux-apt-repo static static/apt/termix-main stable main
-
-# repo url : https://hayinfx.github.io/termix-repo/apt/termix-main
